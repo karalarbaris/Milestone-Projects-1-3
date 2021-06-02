e833c4f8a42b96eae7250a5252106b1425a8d700
Baris <karalarbaris@gmail.com>
2021-06-02 17:21:37 +0200

    Create an app that lists various world flags in a table view. When one of them is tapped, slide in a detail view controller that contains an image view, showing the same flag full size.

---
+++ Milestone-Projects-1-3/DetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailViewController.swift
+//  Milestone-Projects-1-3
+//
+//  Created by Baris Karalar on 2.06.2021.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let img = selectedImage {
+            imageView.image = UIImage(named: img)
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Milestone-Projects-1-3/ViewController.swift
+++ Milestone-Projects-1-3/ViewController.swift
@@ -7,12 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var flags = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+      flags = loadCountryImagesFromDisk()
+        
+    }
+    
+    func loadCountryImagesFromDisk() -> [String] {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        var flags = [String]()
+        
+        for item in items {
+            if item.hasSuffix("@3x.png") {
+//                flags.append(item.replacingOccurrences(of: "@3x.png", with: "").uppercased())
+                flags.append(item)
+            }
+        }
+        
+        return flags
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flags.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = flags[indexPath.row]
+        return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController {
+            vc.selectedImage = flags[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
 }
+
+
 
